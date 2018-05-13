0ab9098cbfc0091e880a821bcfa0f1126e69a44a
Jared Carey <itspandorux@gmail.com>
2018-05-13 18:32:33 +1000

    Created Switch Class, Added General Pressure Plate Functionality to Switch

--- DankMoistCave/Assets/Scripts/PressurePlate.cs
+++ DankMoistCave/Assets/Scripts/PressurePlate.cs
@@ -5,25 +5,8 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class PressurePlate : MonoBehaviour
-{
+public class PressurePlate : Switch {
 
-    [SerializeField]
-    private bool m_isActive = false;
-    [Tooltip("The methods that will be called when the pressure plate is stood on")]
-    public UnityEvent m_On;
-    [Tooltip("The methods that will be called when the pressure plate is stepped off")]
-    public UnityEvent m_Off;
-
-    // Use this for initialization
-    void Start()
-    {
-        if (m_isActive)
-        {
-            InvokeOn();
-        }
-    }
-    
     public bool IsPressed()
     {
         return m_isActive;
@@ -41,13 +24,4 @@ public class PressurePlate : MonoBehaviour
         m_isActive = false;
     }
 
-    public void InvokeOn()
-    {
-        m_On.Invoke();
-    }
-
-    public void InvokeOff()
-    {
-        m_Off.Invoke();
-    }
 }
---
+++ DankMoistCave/Assets/Scripts/Switch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Switch : MonoBehaviour {
+
+    [SerializeField]
+    protected bool m_isActive = false;
+
+    [Tooltip("The methods that will be called when the pressure plate is stood on")]
+    [SerializeField]
+    protected UnityEvent m_On;
+
+    [SerializeField]
+    [Tooltip("The methods that will be called when the pressure plate is stepped off")]
+    protected UnityEvent m_Off;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (m_isActive)
+        {
+            InvokeOn();
+        }
+    }
+
+    public void InvokeOn()
+    {
+        m_On.Invoke();
+    }
+
+    public void InvokeOff()
+    {
+        m_Off.Invoke();
+    }
+}
