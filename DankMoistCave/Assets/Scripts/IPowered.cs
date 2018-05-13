a0b01e0b8e970b5e7b5381f7185e8c69261ce260
Jared Carey <itspandorux@gmail.com>
2018-05-13 15:44:22 +1000

    Added Interfaces and Class Shells
    - IState
    - IPowered
    - PlayerController
    - PressurePlate

---
+++ DankMoistCave/Assets/Scripts/IPowered.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPowered<T> {
+
+    T GetCurrentVoltage();
+    T GetMaxVoltage();
+    void AddPower(T p);
+    void RemovePower(T p);
+
+}
---
+++ DankMoistCave/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour, IState {
+
+    [SerializeField]
+    private bool m_isActive = false;
+    [Tooltip("The methods that will be called when the pressure plate is stood on")]
+    public UnityEvent m_On;
+    [Tooltip("The methods that will be called when the pressure plate is stepped off")]
+    public UnityEvent m_Off;
+
+    // Use this for initialization
+    void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeState()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetState()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetState()
+    {
+        throw new NotImplementedException();
+    }
+}
