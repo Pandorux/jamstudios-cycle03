46a02b29dc4be47715fb37b82456fe4604669b05
Jared Carey <itspandorux@gmail.com>
2018-05-15 07:49:04 +1000

    Created CharacterControls Class

---
+++ DankMoistCave/Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControls : MonoBehaviour {
+
+    public KeyCode[] MoveRight;
+    public KeyCode[] MoveLeft;
+    public KeyCode[] Jump;
+    public KeyCode[] Interaction;
+    public KeyCode[] Pause;
+    public KeyCode[] Restart;
+
+    public bool GetKeysDown(KeyCode[] keycodes)
+    {
+        foreach (KeyCode kc in keycodes)
+        {
+            if (GetKeyDown(kc))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool GetKeyDown(KeyCode keycode)
+    {
+        if (Input.GetKeyDown(keycode))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
