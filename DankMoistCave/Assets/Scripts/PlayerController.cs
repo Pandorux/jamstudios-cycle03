8914262eb1903397f93e12c040dfccaca85fc0f9
Jared Carey <itspandorux@gmail.com>
2018-05-14 19:57:16 +1000

    Partly Implemented PlayerController, Created IHoldable Interface Shell

--- DankMoistCave/Assets/Scripts/PlayerController.cs
+++ DankMoistCave/Assets/Scripts/PlayerController.cs
@@ -3,65 +3,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour, ILight, IDeath
+public class PlayerController : MonoBehaviour
 {
-    public bool CanStateChange()
-    {
-        throw new NotImplementedException();
-    }
+    #region Variables
 
-    public void ChangeState()
-    {
-        throw new NotImplementedException();
-    }
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public static PlayerController instance = null;
 
-    public void DestroyThis()
-    {
-        throw new NotImplementedException();
-    }
+    private bool m_isAlive = true;
 
-    public void Fade(float dur)
-    {
-        throw new NotImplementedException();
-    }
+    [Tooltip("Does the player have its Zippo Lighter out")]
+    [SerializeField]
+    private bool m_LightOn = true;
 
-    public int GetLightIntensity()
-    {
-        throw new NotImplementedException();
-    }
+    [Tooltip("Does the player have their Zippo Lighter")]
+    [SerializeField]
+    private bool m_HasLight = true;
 
-    public LightType GetLightType()
-    {
-        throw new NotImplementedException();
-    }
+    [Tooltip("The body temperature of the player")]
+    [SerializeField]
+    [Range(-20, 65)] // TODO: Look up proper body temps
+    private int m_BodyTemperature = 20;
 
-    public bool GetState()
-    {
-        throw new NotImplementedException();
-    }
+    [Tooltip("Is player holding an item besides the Zippo Lighter?")]
+    [SerializeField]
+    private bool m_HasItem = false;
 
-    public int GetWarmth()
-    {
-        throw new NotImplementedException();
-    }
+    [Tooltip("The Item the player is holding.")]
+    [SerializeField]
+    private IHoldable m_Item = null;
+
+    private PlayerState m_state = PlayerState.Idle;
+
+    #endregion
+
+    #region Class Methods
 
     public bool IsAlive()
     {
-        throw new NotImplementedException();
+        return m_isAlive;
     }
 
-    public void SetAlive(bool a)
+    public bool HasZippo()
     {
-        throw new NotImplementedException();
+        return m_HasLight;
     }
 
-    public void SetLightIntensity(int str)
+    public void LoseZippo()
     {
-        throw new NotImplementedException();
+        m_HasLight = false;
     }
 
-    public void SetWarmth(int w)
+    public int GetBodyTemp()
     {
-        throw new NotImplementedException();
+        return m_BodyTemperature;
     }
+
+    public void ChangeState(PlayerState s)
+    {
+        m_state = s;
+    }
+
+    public void PickUpItem(IHoldable i)
+    {
+        if(m_Item != null)
+            m_Item = i;
+    }
+
+    public IHoldable GetItem()
+    {
+        return m_Item;
+    }
+
+    // TODO: What happens when the item is dropped
+    public void DropItem()
+    {
+        m_Item = null;
+    }
+
+    #endregion
 }
