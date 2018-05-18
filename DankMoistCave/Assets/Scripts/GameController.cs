74249e908f2ada421475225d6b36b91090ba7bcc
Jared Carey <itspandorux@gmail.com>
2018-05-18 12:29:37 +1000

    Added SoundController Functionality from Cycle 02

--- DankMoistCave/Assets/Scripts/GameController.cs
+++ DankMoistCave/Assets/Scripts/GameController.cs
@@ -6,28 +6,32 @@ using UnityEngine.SceneManagement;
 public class GameController : SingletonBase<GameController> {
 
     [Tooltip("The button that'll activate and deactivate the zippo")]
-    public const KeyCode k_ZippoButton = KeyCode.Q;
+    public KeyCode k_ZippoButton = KeyCode.Q;
 
     [Tooltip("The button that'll be used to interact with world objects")]
-    public const KeyCode k_InteractionButton = KeyCode.E;
-
-    protected override void Awake()
-    {
-        base.Awake();
-    }
+    public KeyCode k_InteractionButton = KeyCode.E;
 
     public void PlayerInput()
     {
-        if(Input.GetKeyDown(k_ZippoButton))
+        if (Input.GetKeyDown(k_ZippoButton))
         {
             // Check if Player Can turn Zippo on or off
             // Turn Zippo on or off if possible
         }
-        else if(Input.GetKeyDown(k_InteractionButton))
+        else if (Input.GetKeyDown(k_InteractionButton))
         {
             // Check if player can pickup an item
             // Pickup item if possible
         }
     }
 
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    void Update()
+    {
+        PlayerInput();
+    }
 }
--- DankMoistCave/Assets/Scripts/SoundController.cs
+++ DankMoistCave/Assets/Scripts/SoundController.cs
@@ -4,9 +4,37 @@ using UnityEngine;
 
 public class SoundController : SingletonBase<SoundController> {
 
+    public AudioSource backgroundSource;
+    public AudioSource[] sounds;
+
     protected override void Awake()
     {
         base.Awake();
+
+        backgroundSource.volume = GetMusicVolume();
+
+        foreach (AudioSource sound in sounds)
+        {
+            sound.volume = GetSoundVolume();
+        }
     }
 
+    public void CreateNewSound(AudioClip sound, bool loopSound = false)
+    {
+        GameObject obj = new GameObject();
+        obj.transform.parent = transform;
+        obj.AddComponent<AudioSource>();
+        obj.GetComponent<AudioSource>().clip = sound;
+        obj.GetComponent<AudioSource>().loop = loopSound;
+        obj.GetComponent<AudioSource>().volume *= GetSoundVolume();
+        obj.GetComponent<AudioSource>().Play();
+
+        if (!loopSound)
+            Destroy(obj, sound.length);
+    }
+
+    public float ReturnSoundVolume()
+    {
+        return GetSoundVolume();
+    }
 }
