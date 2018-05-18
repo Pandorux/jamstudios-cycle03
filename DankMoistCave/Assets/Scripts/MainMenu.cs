7cc253c22e317cdd1c54246edb4d83911da9a941
Jared Carey <itspandorux@gmail.com>
2018-05-18 12:45:15 +1000

    Added Saveable Data and MainMenu Scripts from Cycle 02

---
+++ DankMoistCave/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : SaveableData {
+
+    public Slider soundVol;
+    public Slider musicVol;
+
+    void Start()
+    {
+        soundVol.value = GetSoundVolume();
+        musicVol.value = GetMusicVolume();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SaveSoundVolume()
+    {
+        SetSoundVolume(soundVol.value);
+    }
+
+    public void SaveMusicVolume()
+    {
+        SetMusicVolume(musicVol.value);
+    }
+
+}
---
+++ DankMoistCave/Assets/Scripts/SaveableData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SaveableData : MonoBehaviour {
+
+    #region Data Persistence
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    protected float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat("musicVolume", 1);
+    }
+
+    /// <summary>
+    ///  
+    /// </summary>
+    /// <param name="isCompleted"></param>
+    protected static void SetMusicVolume(float curVolume)
+    {
+        PlayerPrefs.SetFloat("musicVolume", curVolume);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    protected static float GetSoundVolume()
+    {
+        return PlayerPrefs.GetFloat("soundVolume", 1);
+    }
+
+    /// <summary>
+    ///  
+    /// </summary>
+    /// <param name="isCompleted"></param>
+    protected static void SetSoundVolume(float curVolume)
+    {
+        PlayerPrefs.SetFloat("soundVolume", curVolume);
+    }
+
+    #endregion
+
+
+}
