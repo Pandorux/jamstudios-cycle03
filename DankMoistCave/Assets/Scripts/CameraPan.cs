ea5485049d6deabfabbb18dd2542ac0c57018fb8
Unknown <melikehatcliff@hotmail.com>
2018-06-04 03:16:16 +1000

    Level03 1

---
+++ DankMoistCave/Assets/Scripts/CameraPan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+    // variable for the main camera attached to the player
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //change y position to be -4 units from its original position when in the trigger. Upon exiting the view should go back to normal.
+    }
+}
