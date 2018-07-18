6c437c3e1bf13875bb3ff9899bba5b9b920bfbf4
Jared Carey <itspandorux@gmail.com>
2018-07-18 15:57:20 +1000

    Moving Between Caves Implemented

---
+++ DankMoistCave/Assets/Scripts/Teleport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public Vector3 position;
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            c.transform.position = position;
+        }
+    }
+}
